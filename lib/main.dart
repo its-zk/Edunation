5cb672097f3aecf32031f458fd53bb1630a2a72e
Hassan Ahmad <dev.hassanahmad12@gmail.com>
2023-07-15 23:31:09 +0500

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:edunation/firebase_options.dart';
 import 'package:edunation/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
